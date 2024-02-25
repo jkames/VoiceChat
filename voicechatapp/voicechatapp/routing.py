ac9a702803378d84535ed0495afdd8766996c7dc
jkames <jkamess5@gmail.com>
2024-02-25 11:30:41 -0600

    websocket works

--- voicechatapp/voicechatapp/routing.py
+++ voicechatapp/voicechatapp/routing.py
@@ -1,6 +1,6 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from voicechatapp import consumers
+from . import consumers
 
 application = ProtocolTypeRouter({
     'websocket': URLRouter([
